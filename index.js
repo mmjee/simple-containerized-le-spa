83bf599f87d7f99ba227b3710cb4e677ca85b771
root <root@git.maharshi.ninja>
2021-08-25 03:25:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const routing = require('./app')
+
+// require("greenlock-express")
+require('@root/greenlock-express')
+  .init({
+    packageRoot: __dirname,
+    configDir: './greenlock.d',
+
+    cluster: false
+  })
+  .ready(httpsWorker)
+
+function httpsWorker (glx) {
+  //
+  // HTTP2 would have been the default httpsServer for node v12+
+  // However... https://github.com/expressjs/express/issues/3388
+  //
+
+  // Get the raw http2 server:
+  const tlsOptions = null
+  const http2Server = glx.http2Server(tlsOptions, routing)
+
+  http2Server.listen(443, '::', function () {
+    console.info('Listening on ', http2Server.address())
+  })
+
+  // Note:
+  // You must ALSO listen on port 80 for ACME HTTP-01 Challenges
+  // (the ACME and http->https middleware are loaded by glx.httpServer)
+  const httpServer = glx.httpServer()
+
+  httpServer.listen(80, '::', function () {
+    console.info('Listening on ', httpServer.address())
+  })
+}
